63c6b8d23b27ff587a575b69bcde92c6ec35aeac
Yusufa Haidar <haidardewa@gmail.com>
2024-04-24 15:46:21 +0700

    Perfecting Seeders for Distribusi

--- database/seeders/DistribusiSeeder.php
+++ database/seeders/DistribusiSeeder.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-class StatusBarangSeeder extends Seeder
+class DistribusiSeeder extends Seeder
 {
     /**
      * Run the database seeds.
