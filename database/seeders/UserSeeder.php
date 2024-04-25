16cdb72a98389d1cb85551450cd3f69422009309
Echa Aprilya <echaaprilya044@gmail.com>
2024-04-25 14:53:17 +0700

    Edit userSeeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,24 +15,60 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         $data = [
+            // [
+            //     'id_user' => 1,
+            //     'id_role' => 1,
+            //     'nama' => 'Yusufa Haidar',
+            //     'email' => 'haidardewa@gmail.com',
+            //     'no_hp' => '081234567890',
+            //     'username' => 'RyuzeiGG',
+            //     'password' => Hash::make('123456')
+            // ],
+            // [
+            //     'id_user' => 2,
+            //     'id_role' => 2,
+            //     'nama' => 'Richard Santana',
+            //     'email' => 'RSSantana@yahoo.com',
+            //     'no_hp' => '087569321458',
+            //     'username' => 'RSSantana',
+            //     'password' => Hash::make('123456')
+            // ],
             [
-                'id_user' => 1,
+                'id_user' => 3,
                 'id_role' => 1,
-                'nama' => 'Yusufa Haidar',
-                'email' => 'haidardewa@gmail.com',
+                'nama' => 'Gilang Pambudi',
+                'email' => 'gilang@test.com',
                 'no_hp' => '081234567890',
-                'username' => 'RyuzeiGG',
+                'username' => 'gilang',
                 'password' => Hash::make('123456')
             ],
             [
-                'id_user' => 2,
-                'id_role' => 2,
-                'nama' => 'Richard Santana',
-                'email' => 'RSSantana@yahoo.com',
-                'no_hp' => '087569321458',
-                'username' => 'RSSantana',
+                'id_user' => 4,
+                'id_role' => 1,
+                'nama' => 'Echa Apriliya',
+                'email' => 'echa@test.com',
+                'no_hp' => '081234567890',
+                'username' => 'echa',
                 'password' => Hash::make('123456')
-            ]
+            ],
+            [
+                'id_user' => 5,
+                'id_role' => 1,
+                'nama' => 'Anisna Hilwa',
+                'email' => 'anisna@test.com',
+                'no_hp' => '081234567890',
+                'username' => 'anisna',
+                'password' => Hash::make('123456')
+            ],
+            [
+                'id_user' => 6,
+                'id_role' => 1,
+                'nama' => 'Maulana Akbar',
+                'email' => 'akbar@test.com',
+                'no_hp' => '081234567890',
+                'username' => 'akbar',
+                'password' => Hash::make('123456')
+            ],
         ];
         DB::table('users')->insert($data);
     }
